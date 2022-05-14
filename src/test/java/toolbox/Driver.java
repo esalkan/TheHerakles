11b1d53bed77891eac9abee31d6acfdec442b5e5
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-05-15 01:09:56 +0300

    Comments added

--- src/test/java/toolbox/Driver.java
+++ src/test/java/toolbox/Driver.java
@@ -18,61 +18,61 @@ import org.openqa.selenium.safari.SafariDriver;
 
 
 public class Driver {
-    // Constructor
+    // Constructor for driver.
     private Driver() {
     }
 
-    // for reaching to the WebDriver from everywhere.
+    // for reaching to the WebDriver from everywhere. It's a singleton.  It's a static variable.
     private static WebDriver driver;
 
-    // Instance getDriver(browserType) method for driver.
+    // Instance getDriver(browserType) method for driver. It's a static method.
     public static WebDriver getDriver(String browserType) {
-        // Adding condition before driver type selection
+        // Adding condition before driver type selection to avoid null pointer exception.
         if (driver == null) {
-            // Based on browser selection setup browser driver type.
+            // Based on browser selection setup browser driver type. It's a switch case.
             switch (browserType) {
-                // Chrome browser WebDriver setup
+                // Chrome browser WebDriver setup for Chrome browser.
                 case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    WebDriverManager.chromedriver().setup(); // Chrome driver setup.
+                    driver = new ChromeDriver(); // Chrome driver initialization.
                     break;
                 // Firefox browser WebDriver setup
                 case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                    WebDriverManager.firefoxdriver().setup(); // Firefox driver setup.
+                    driver = new FirefoxDriver(); // Firefox driver initialization.
                     break;
                 // Internet Explorer browser WebDriver setup
                 case "ie":
-                    if (!System.getProperty("os.name").toLowerCase().contains("windows")) {
-                        throw new WebDriverException("Your OS doesn't support Internet Explorer");
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows")) { // If OS is not Windows.
+                        throw new WebDriverException("Your OS doesn't support Internet Explorer"); // Throw exception.
                     }
-                    WebDriverManager.iedriver().setup();
-                    driver = new InternetExplorerDriver();
+                    WebDriverManager.iedriver().setup(); // Internet Explorer driver setup.
+                    driver = new InternetExplorerDriver(); // Internet Explorer driver initialization.
                     break;
                 // Edge browser WebDriver setup
                 case "edge":
                     // Checking system os type and throw exception with message if the browser type is not match with system.
-                    if (!System.getProperty("os.name").toLowerCase().contains("windows")) {
-                        throw new WebDriverException("Your OS doesn't support Edge");
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows")) { // If OS is not Windows.
+                        throw new WebDriverException("Your OS doesn't support Edge"); // Throw exception.
                     }
-                    WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
+                    WebDriverManager.edgedriver().setup(); // Edge driver setup.
+                    driver = new EdgeDriver(); // Edge driver initialization.
                     break;
                 // Safari browser WebDriver setup
                 case "safari":
                     // Checking system os type and throw exception with message if the browser type is not match with system.
-                    if (!System.getProperty("os.name").toLowerCase().contains("mac")) {
-                        throw new WebDriverException("Your OS doesn't support Safari");
+                    if (!System.getProperty("os.name").toLowerCase().contains("mac")) { // If OS is not Mac.
+                        throw new WebDriverException("Your OS doesn't support Safari"); // Throw exception.
                     }
-                    WebDriverManager.getInstance(SafariDriver.class).setup();
-                    driver = new SafariDriver();
+                    WebDriverManager.getInstance(SafariDriver.class).setup(); // Safari driver setup.
+                    driver = new SafariDriver(); // Safari driver initialization.
                     break;
             }
         }
-        return driver;
+        return driver; // Return driver.
     }
 
-    // After test step definitions killing the driver and make web driver null
+    // Instance killDriver() method for driver.
     public static void killDriver() {
         if (driver != null) {
             driver.quit();
